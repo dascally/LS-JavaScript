b46869c9af26f2ccc3841b0fb3d1623bbe561e7a
Danny Scally <44758401+dascally@users.noreply.github.com>
2025-10-30 17:28:13 -0400

    [Ch1]: Complete exercise 3

---
+++ Ch1-TheBasics/3.js
@@ -0,0 +1,7 @@
+console.log(`Type of 'true' is: ${typeof 'true'}`);
+console.log(`Type of false is: ${typeof false}`);
+console.log(`Type of 1.5 is: ${typeof 1.5}`);
+console.log(`Type of 2 is: ${typeof 2}`);
+console.log(`Type of undefined is: ${typeof undefined}`);
+console.log(`Type of { foo: 'bar' } is: ${typeof { foo: 'bar' }}`);
+
